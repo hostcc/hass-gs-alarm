6dc1663077c17f017484332505874bd5a5267b4d
Ilia Sotnikov <hostcc@gmail.com>
2022-11-26 11:42:31 +0200

    * Tests: added shared `mock_g90alarm` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,123 @@
+"""
+tbd
+"""
+from unittest.mock import patch, AsyncMock, PropertyMock
 import pytest
 
+import pyg90alarm
+
+
 @pytest.fixture(autouse=True)
+# pylint: disable=unused-argument
 def auto_enable_custom_integrations(enable_custom_integrations):
+    """
+    tbd
+    """
     yield
+
+
+def pytest_configure(config):
+    """
+    tbd
+    """
+    config.addinivalue_line("markers", "g90discovery")
+
+
+@pytest.fixture
+def mock_g90alarm(request):
+    """
+    tbd
+    """
+    with (
+        patch('custom_components.gs_alarm.G90Alarm', autospec=True) as mock,
+        patch(
+            'custom_components.gs_alarm.config_flow.G90Alarm.discover'
+        ) as mock_discover,
+    ):
+        mock_discover.return_value = (
+            getattr(
+                request.node
+                .get_closest_marker('g90discovery'),
+                'kwargs', {}
+            ).get('result', [])
+        )
+
+        mock.return_value.get_host_info = AsyncMock(
+            return_value=pyg90alarm.alarm.G90HostInfo(
+                host_guid='Dummy',
+                product_name='Dummy product',
+                wifi_protocol_version='1.0-test',
+                cloud_protocol_version='1.1-test',
+                mcu_hw_version='1.0-test',
+                wifi_hw_version='1.0-test',
+                gsm_status_data=0,
+                wifi_status_data=0,
+                reserved1=None,
+                reserved2=None,
+                band_frequency=None,
+                gsm_signal_level=100,
+                wifi_signal_level=100,
+            )
+        )
+
+        mock.return_value.get_host_status = AsyncMock(
+            return_value=pyg90alarm.alarm.G90HostStatus(
+                host_status=pyg90alarm.alarm.G90ArmDisarmTypes.DISARM,
+                host_phone_number=None,
+                product_name='Dummy product',
+                mcu_hw_version='1.0',
+                wifi_hw_version='1.0',
+            )
+        )
+
+        mock_sensor = pyg90alarm.alarm.G90Sensor(
+            parent=mock.return_value, subindex=0, proto_idx=0,
+            parent_name='Dummy sensor',
+            index=0,
+            room_id=0,
+            type_id=1,
+            subtype=0,
+            timeout=0,
+            user_flag_data=255,
+            baudrate=0,
+            protocol_id=0,
+            reserved_data=0,
+            node_count=0,
+            mask=0,
+            private_data=0,
+        )
+        mock_sensor.set_enabled = AsyncMock()
+        type(mock_sensor).supports_enable_disable = (
+            PropertyMock(return_value=True)
+        )
+
+        mock.return_value.get_sensors = AsyncMock(
+            return_value=[
+                mock_sensor,
+            ]
+        )
+
+        mock.return_value.get_devices = AsyncMock(
+            return_value=[
+                pyg90alarm.alarm.G90Device(
+                    parent=mock.return_value, subindex=0, proto_idx=0,
+                    parent_name='Dummy switch',
+                    index=0,
+                    room_id=0,
+                    type_id=0,
+                    subtype=0,
+                    timeout=0,
+                    user_flag_data=0,
+                    baudrate=0,
+                    protocol_id=0,
+                    reserved_data=0,
+                    node_count=0,
+                    mask=0,
+                    private_data=0,
+                )
+            ]
+        )
+
+        mock.return_value.listen_device_notifications = AsyncMock()
+
+        yield mock
--- tests/test_init.py
+++ tests/test_init.py
@@ -2,8 +2,6 @@
 tbd
 """
 from datetime import timedelta
-from unittest.mock import patch, AsyncMock
-
 from pytest_unordered import unordered
 
 from pytest_homeassistant_custom_component.common import (
@@ -14,8 +12,6 @@ from homeassistant.util import dt
 import homeassistant.helpers.device_registry as dr
 import homeassistant.helpers.entity_registry as er
 
-import pyg90alarm
-
 from custom_components.gs_alarm import (
     async_setup_entry,
     async_unload_entry,
@@ -23,7 +19,7 @@ from custom_components.gs_alarm import (
 from custom_components.gs_alarm.const import DOMAIN
 
 
-async def test_setup_unload_and_reload_entry(hass):
+async def test_setup_unload_and_reload_entry(hass, mock_g90alarm):
     """
     tbd
     """
@@ -33,102 +29,31 @@ async def test_setup_unload_and_reload_entry(hass):
         options={'disabled_sensors': ['Dummy sensor#1']},
         entry_id="test"
     )
-    config_entry.add_to_hass(hass)
-    with patch('custom_components.gs_alarm.G90Alarm', autospec=True) as mock:
-        mock.return_value.get_host_info = AsyncMock(
-            return_value=pyg90alarm.alarm.G90HostInfo(
-                host_guid='Dummy',
-                product_name='Dummy product',
-                wifi_protocol_version='1.0-test',
-                cloud_protocol_version='1.1-test',
-                mcu_hw_version='1.0-test',
-                wifi_hw_version='1.0-test',
-                gsm_status_data=0,
-                wifi_status_data=0,
-                reserved1=None,
-                reserved2=None,
-                band_frequency=None,
-                gsm_signal_level=100,
-                wifi_signal_level=100,
-            )
-        )
 
-        mock.return_value.get_host_status = AsyncMock(
-            return_value=pyg90alarm.alarm.G90HostStatus(
-                host_status=pyg90alarm.alarm.G90ArmDisarmTypes.DISARM,
-                host_phone_number=None,
-                product_name='Dummy product',
-                mcu_hw_version='1.0',
-                wifi_hw_version='1.0',
-            )
-        )
+    assert await async_setup_entry(hass, config_entry)
+    async_fire_time_changed(hass, dt.utcnow() + timedelta(hours=1))
+    await hass.async_block_till_done()
 
-        mock.return_value.get_sensors = AsyncMock(
-            return_value=[
-                pyg90alarm.alarm.G90Sensor(
-                    parent=mock.return_value, subindex=0, proto_idx=0,
-                    parent_name='Dummy sensor',
-                    index=0,
-                    room_id=0,
-                    type_id=1,
-                    subtype=0,
-                    timeout=0,
-                    user_flag_data=255,
-                    baudrate=0,
-                    protocol_id=0,
-                    reserved_data=0,
-                    node_count=0,
-                    mask=0,
-                    private_data=0,
-                )
-            ]
-        )
-        mock.return_value.get_devices = AsyncMock(
-            return_value=[
-                pyg90alarm.alarm.G90Device(
-                    parent=mock.return_value, subindex=0, proto_idx=0,
-                    parent_name='Dummy switch',
-                    index=0,
-                    room_id=0,
-                    type_id=0,
-                    subtype=0,
-                    timeout=0,
-                    user_flag_data=0,
-                    baudrate=0,
-                    protocol_id=0,
-                    reserved_data=0,
-                    node_count=0,
-                    mask=0,
-                    private_data=0,
-                )
-            ]
-        )
+    mock_g90alarm.assert_called_once_with(host='dummy-ip')
 
-        mock.return_value.listen_device_notifications = AsyncMock()
-        assert await async_setup_entry(hass, config_entry)
-        async_fire_time_changed(hass, dt.utcnow() + timedelta(hours=1))
-        await hass.async_block_till_done()
+    integration_devices = dr.async_entries_for_config_entry(
+        dr.async_get(hass), config_entry.entry_id
+    )
+    assert len(integration_devices) == 1
+    integration_device_id = integration_devices[0].id
 
-        mock.assert_called_once_with(host='dummy-ip')
+    integration_entities = er.async_entries_for_device(
+        er.async_get(hass), integration_device_id
+    )
+    integration_entity_ids = [x.entity_id for x in integration_entities]
+    assert integration_entity_ids == unordered([
+        'alarm_control_panel.dummy',
+        'binary_sensor.dummy_sensor_1',
+        'switch.dummy_switch_1',
+        'sensor.gs_alarm_wifi_signal',
+        'binary_sensor.gs_alarm_wifi_status',
+        'sensor.gs_alarm_gsm_signal',
+        'binary_sensor.gs_alarm_gsm_status'
+    ])
 
-        integration_devices = dr.async_entries_for_config_entry(
-            dr.async_get(hass), config_entry.entry_id
-        )
-        assert len(integration_devices) == 1
-        integration_device_id = integration_devices[0].id
-
-        integration_entities = er.async_entries_for_device(
-            er.async_get(hass), integration_device_id
-        )
-        integration_entity_ids = [x.entity_id for x in integration_entities]
-        assert integration_entity_ids == unordered([
-            'alarm_control_panel.dummy',
-            'binary_sensor.dummy_sensor_1',
-            'switch.dummy_switch_1',
-            'sensor.gs_alarm_wifi_signal',
-            'binary_sensor.gs_alarm_wifi_status',
-            'sensor.gs_alarm_gsm_signal',
-            'binary_sensor.gs_alarm_gsm_status'
-        ])
-
-        assert await async_unload_entry(hass, config_entry)
+    assert await async_unload_entry(hass, config_entry)
