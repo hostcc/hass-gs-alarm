ece8dd019d5e5322ebda5723ff5e4a2876dbfe0a
Ilia Sotnikov <hostcc@gmail.com>
2024-08-21 01:34:47 +0300

    Diagnostics support
    * Added support for downloading diagnostics from device or config entry

---
+++ custom_components/gs_alarm/diagnostics.py
@@ -0,0 +1,102 @@
+"""
+Diagnostics support for the integration.
+"""
+from __future__ import annotations
+from typing import Any
+
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.device_registry import DeviceEntry
+from homeassistant.helpers.redact import async_redact_data
+from pyg90alarm.entities.sensor import G90Sensor
+from pyg90alarm.entities.device import G90Device
+from pyg90alarm.exceptions import G90Error, G90TimeoutError
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+# Redact the properties could contain sensitive information
+TO_REDACT = [
+    'host_guid', 'host_phone_number', 'ip_addr', 'serial_number', 'name',
+    'title', 'identifiers', 'extra_data',
+]
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """
+    Returns diagnostics for the config entry.
+    """
+    return await async_get_device_diagnostics(hass, entry, None)
+
+
+def format_g90_sensor_device(sensor: G90Sensor | G90Device) -> dict[str, Any]:
+    """
+    Formats G90Sensor or G90Device returning `dict` representing its
+    properties.
+
+    """
+    return {
+        'name': sensor.name,
+        'type': repr(sensor.type),
+        # pylint:disable=protected-access
+        'subtype': sensor._protocol_data.subtype,
+        'index': sensor.index,
+        'subindex': sensor.subindex,
+        'node_count': sensor.node_count,
+        'protocol': repr(sensor.protocol),
+        'occupancy': sensor.occupancy,
+        'user_flag': repr(sensor.user_flag),
+        'reserved': repr(sensor.reserved),
+        'extra_data': repr(sensor.extra_data),
+        'enabled': repr(sensor.enabled),
+        'supports_enable_disable': sensor.supports_enable_disable,
+    }
+
+
+async def async_get_device_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry, device: DeviceEntry
+) -> dict[str, Any]:
+    """
+    Returns diagnostics for the device entry.
+    """
+
+    try:
+        hass_data = hass.data[DOMAIN][entry.entry_id]
+        g90_client = hass_data['client']
+
+        result = {
+            'config_entry': entry.as_dict(),
+            'device_entry': device.dict_repr if device else None,
+            'alarm_panel': {
+                'history': [
+                    repr(x)
+                    # 50 history records should be the maximum for most of the
+                    # panels
+                    for x in await g90_client.history(count=50)
+                ],
+                'sensors': [
+                    format_g90_sensor_device(x)
+                    for x in await g90_client.get_sensors()
+                ],
+                'devices': [
+                    format_g90_sensor_device(x)
+                    for x in await g90_client.get_devices()
+                ],
+                'host_info': (await g90_client.get_host_info())._asdict(),
+                'host_status': (await g90_client.get_host_status())._asdict(),
+                'alert_config': repr(await g90_client.get_alert_config()),
+                # pylint:disable=protected-access
+                'alert_simulation_task':
+                    repr(g90_client._alert_simulation_task),
+            },
+        }
+
+        return async_redact_data(result, TO_REDACT)
+    # Errors raised by `pyg90alarm` are treated as non-terminating, just
+    # resulting in error response
+    except (G90Error, G90TimeoutError) as exc:
+        msg = f'Unable to gather diagnostics data: {repr(exc)}'
+        _LOGGER.error(msg)
+        return {'error': msg}
---
+++ tests/test_diagnostics.py
@@ -0,0 +1,104 @@
+"""
+Tests for integration diagnostics.
+"""
+import pytest
+from pytest_unordered import unordered
+from pytest_homeassistant_custom_component.common import (
+    MockConfigEntry,
+)
+from homeassistant.setup import async_setup_component
+import homeassistant.helpers.device_registry as dr
+from pyg90alarm.exceptions import G90Error
+
+from custom_components.gs_alarm.const import DOMAIN
+
+
+@pytest.mark.usefixtures('mock_g90alarm')
+async def test_diagnostics(hass, hass_client):
+    """
+    Verifies diagnostics are successfully generated.
+    """
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={'ip_addr': 'dummy-ip'},
+        options={},
+        entry_id="test-diag"
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    # Add `diagnostics` integration
+    await async_setup_component(hass, "diagnostics", {})
+    await hass.async_block_till_done()
+
+    integration_devices = dr.async_entries_for_config_entry(
+        dr.async_get(hass), config_entry.entry_id
+    )
+    integration_device_id = next(iter(integration_devices)).id
+
+    # Keys expected for the response 
+    expected_data_keys = unordered([
+        'config_entry', 'device_entry', 'alarm_panel'
+    ])
+    # And its `alarm_panel` nested element
+    expected_alarm_panel_keys = unordered([
+        'history',
+        'host_info',
+        'host_status',
+        'sensors',
+        'devices',
+        'alert_config',
+        'alert_simulation_task'
+    ])
+
+    # Simulate diagnostics download for the config entry
+    client = await hass_client()
+    response = await client.get(
+        f"/api/diagnostics/config_entry/{config_entry.entry_id}"
+    )
+    # Verify the response
+    response_dict = await response.json()
+    data = response_dict.get('data')
+    assert list(data.keys()) == expected_data_keys
+    assert list(data['alarm_panel'].keys()) == expected_alarm_panel_keys
+
+    # Same but for the device
+    response = await client.get(
+       f"/api/diagnostics/config_entry"
+       f"/{config_entry.entry_id}/device/{integration_device_id}"
+    )
+    response_dict = await response.json()
+    data = response_dict.get('data')
+
+    assert list(data.keys()) == expected_data_keys
+    assert list(data['alarm_panel'].keys()) == expected_alarm_panel_keys
+
+
+async def test_diagnostics_exception(hass, hass_client, mock_g90alarm):
+    """
+    Verify the `pyg90alarm` error doesn't lead to diagnostics resulting in
+    internal error sent back to the client.
+    """
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={'ip_addr': 'dummy-ip'},
+        options={},
+        entry_id="test-diag"
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await async_setup_component(hass, "diagnostics", {})
+    await hass.async_block_till_done()
+
+    # Simulate exception raised from `G90Alarm.history()`
+    mock_g90alarm.return_value.history.side_effect = G90Error()
+
+    client = await hass_client()
+    response = await client.get(
+        f"/api/diagnostics/config_entry/{config_entry.entry_id}"
+    )
+    # Verify the response
+    response_dict = await response.json()
+    data = response_dict.get('data')
+    assert data == {
+        'error': 'Unable to gather diagnostics data: G90Error()'
+    }
